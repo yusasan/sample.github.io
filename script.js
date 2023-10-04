a008c6c311b870d198186969dc587fe2313bbe0d
yusasan <145940414+yusasan@users.noreply.github.com>
2023-10-04 11:47:07 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+var startButton;    // startボタン
+var stopButton;     // stopボタン
+var resetButton;    // resetボタン
+var showTime;       // 表示時間
+
+var timer;              // setinterval, clearTimeoutで使用
+var startTime;          // 開始時間
+var elapsedTime = 0;    // 経過時間
+var holdTime = 0;       // 一時停止用に時間を保持
+
+window.onload = function () {
+    startButton = document.getElementById("start");
+    stopButton = document.getElementById("stop");
+    resetButton = document.getElementById("reset");
+    showTime = document.getElementById("time");
+}
+
+// スタートボタン押下時
+function start(){
+    // 開始時間を現在の時刻に設定
+    startTime = Date.now();
+
+    // 時間計測
+    measureTime();
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = false;
+}
+
+// ストップボタン押下時
+function stop(){
+    // タイマー停止
+    clearInterval(timer);
+
+    // 停止時間を保持
+    holdTime += Date.now() - startTime;
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+}
+
+// リセットボタン押下時
+function reset(){
+    // タイマー停止
+    clearInterval(timer);
+
+    // 変数、表示を初期化
+    elapsedTime = 0;
+    holdTime = 0;
+    showTime.textContent = "00:00:00:0";
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = true;
+}
+
+// 時間を計測（再帰関数）
+function measureTime() {
+    // タイマーを設定
+    timer = setTimeout(function () {
+        // 経過時間を設定し、画面へ表示
+        elapsedTime = Date.now() - startTime + holdTime;
+        showTime.textContent = new Date(elapsedTime).toISOString().slice(11, 21);
+        
+        // 関数を呼び出し、時間計測を継続する
+        measureTime();
+    }, 10);
+}
